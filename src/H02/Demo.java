9619cef8e703024e55c4481a04691bc9f227ea62
Mano <rtorbed@roc-dev.com>
2019-09-03 13:41:14 +0200

    gecommit

---
+++ src/H02/Demo.java
@@ -0,0 +1,14 @@
+package H02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Demo extends Applet {
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.drawString( "hh", 40, 40);
+
+    }
+}
